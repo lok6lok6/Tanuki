c0263fdd8d8c454d1ac74b0ebd5c33f7d8e24f47
Myles Slack <binglaplap@gmail.com>
2025-02-14 17:17:39 -0800

    Initial commit: Xcode project setup

---
+++ Tanuki/Tanuki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tanuki
+//
+//  Created by Myles Slack on 2025.02.14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tanuki/Tanuki/TanukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanukiApp.swift
+//  Tanuki
+//
+//  Created by Myles Slack on 2025.02.14.
+//
+
+import SwiftUI
+
+@main
+struct TanukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
